90d5ae0a93029c93e0025e73b92c8af2f415cb0f
Marcis Bergmanis <marcisbergmanis@gmail.com>
2025-03-27 16:41:03 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
 	reporter: "html",
 	testMatch: ["*/tests/**/*.ts", "*/**/setup.ts"],
-	timeout: 3 * 1000,
+	timeout: 30 * 1000,
 	fullyParallel: false,
 	workers: 1,
 	projects: [
