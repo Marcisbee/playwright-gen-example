57032e3c13fc65bf19f3951dcab28fc136dc11d6
Marcis Bergmanis <marcisbergmanis@gmail.com>
2025-03-31 11:11:32 +0300

    playwright with deno

---
+++ task.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# ./tasks test --grep="/pocketbase/"
+task_test() {
+  start_task prepare
+
+  ./deno run --allow-all npm:playwright@1.51.1 test --trace=on "${@}"
+}
+
+# ./tasks codegen https://demo.playwright.dev/todomvc --output="./todo-mvc-3.ts"
+task_codegen() {
+  start_task prepare
+
+  ./deno run --allow-all npm:playwright@1.51.1 codegen --target="playwright-test" "${@}"
+}
+
+# --- PREPARE ---
+task_prepare() {
+    if [ ! -f "./deno" ]; then
+        echo "Installing deno"
+        curl -SsfL https://marcisbee.github.io/gh/dl.sh | bash -s -- --repo denoland/deno --tag v2.2.6
+    else
+        echo "Found deno"
+    fi
+
+    ./deno --allow-all npm:playwright@1.51.1 install
+}
